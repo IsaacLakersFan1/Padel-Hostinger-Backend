bb3d846f6cbddb2d328457094844d345be3b1fa4
Isaac Salazar <isaacsalazar9911@gmail.com>
2025-02-20 11:00:56 -0600

    Fix: download DB

--- src/controllers/settingsController.ts
+++ src/controllers/settingsController.ts
@@ -16,6 +16,8 @@ const downloadDB = async (req: Request, res: Response): Promise<void> => {
     res.setHeader("Content-Disposition", 'attachment; filename="database.db"');
     res.setHeader("Content-Type", "application/x-sqlite3");
     res.setHeader("Content-Length", fs.statSync(DB_PATH).size.toString());
+    res.setHeader("Content-Security-Policy", "frame-ancestors 'self' http://gkks0gc4c4g4gc4soo0osg08.193.46.198.43.sslip.io");
+
 
     const fileStream = fs.createReadStream(DB_PATH);
     fileStream.pipe(res);
